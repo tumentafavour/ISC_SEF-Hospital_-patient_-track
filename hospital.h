207c36df5f00bc73c4867f82bc015a512666f3e7
tumentafavour <tumentafavour6@gmail.com>
2025-08-07 14:47:03 +0100

    feat: I declared all functions I will be using

--- hospital.h
+++ hospital.h
@@ -1,4 +1,15 @@
-#include <stdio.h>
+#ifdef HOSPITAL_H
+#define HOSPITAL_H
+
+typedef struct {
+    int id;
+    char name[50];
+    char password[20];
+} Admin;
+typedef struct {
+char name[50];
+char specialization[50];
+} Doctor;
 
 typedef struct {
 int id;
@@ -9,4 +20,12 @@ char status[20]; // "Admitted" or "Discharged"
 } Patient;
 
 
-
+void save_and_exit();
+void load_records();
+void displplayMenu();
+void adminLogin();
+void registerPatient();
+void viewPatients();
+void searchbyID();
+void updatePatientInfo();
+void dischargePatient();
