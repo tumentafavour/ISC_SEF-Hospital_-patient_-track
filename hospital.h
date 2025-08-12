653ad6ab8b3f768ecf62dcbd39cac0e5a6c7cfc0
Reece-Sam <bonisamkelekoh@gmail.com>
2025-08-12 12:11:27 +0100

    fix : changed hospital.h

--- hospital.h
+++ hospital.h
@@ -1,14 +1,17 @@
-#ifdef HOSPITAL_H
+#ifndef HOSPITAL_H
 #define HOSPITAL_H
 
-typedef struct {
+typedef struct
+{
     int id;
     char name[50];
     char password[20];
 } Admin;
-typedef struct {
-char name[50];
-char specialization[50];
+typedef struct
+{
+    char name[50];
+    char specialization[50];
+    char date[12];
 } Doctor;
 
 typedef struct
@@ -18,7 +21,19 @@ typedef struct
     int age;
     char diagnosis[100];
     char status[20]; // "Admitted" or "Discharged"
+    char date[12];
 } Patient;
 
+int main()
+{
+    int d;
 
+    while (1)
+    {
+        system("clear");
+        printf("...Welcome to this Hospital system...\n");
+        printf("1. Admit Patient\n");
+        printf("Patient list\n");
+        printf("")
 
+#endif
