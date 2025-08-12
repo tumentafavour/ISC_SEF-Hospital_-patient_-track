eaf1f1ee1ee994d956cafaf35be1549bba874220
Reece-Sam <bonisamkelekoh@gmail.com>
2025-08-12 13:34:54 +0100

    I defined the functions for hospital.h

--- hospital.c
+++ hospital.c
@@ -1,2 +1,12 @@
-
-
+#include "hospital.h"
+void DisplayMenu()
+{
+    printf("...Main Menu...\n");
+    printf("1. Register patient\n");
+    printf("2. View patient\n");
+    printf("3. Search by ID\n");
+    printf("4. Update patient info\n");
+    printf("5. Discharge patient\n");
+    printf("6. Enter your option");
+    printf("7. Save and Exit\n");
+}
